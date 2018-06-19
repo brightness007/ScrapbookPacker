5e7424194865d571f47e899b66f4926447221d4b
zhangpeng <zhangpeng_vip@hotmail.com>
2018-06-19 23:15:56 +0800

    * Pack ScrapBook Folders into ZIP Files (Drag & Drop)

---
+++ Source/ScrapbookPacker/frmMain.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace ScrapbookPacker
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Log = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_Log
+            // 
+            this.textBox_Log.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_Log.Location = new System.Drawing.Point(4, 4);
+            this.textBox_Log.Multiline = true;
+            this.textBox_Log.Name = "textBox_Log";
+            this.textBox_Log.Size = new System.Drawing.Size(792, 442);
+            this.textBox_Log.TabIndex = 0;
+            // 
+            // frmMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_Log);
+            this.Name = "frmMain";
+            this.Padding = new System.Windows.Forms.Padding(4);
+            this.Text = "Scrapbook Packer";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmMain_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmMain_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Log;
+    }
+}
+
---
+++ Source/ScrapbookPacker/frmMain.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using Wiry.Base32;
+
+namespace ScrapbookPacker
+{
+    public partial class frmMain : Form
+    {
+        private string _sRootFolder;
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            String sAppPath = new Uri(Path.GetDirectoryName(Assembly.GetExecutingAssembly().
+                GetName().CodeBase)).LocalPath;
+            _sRootFolder = Path.GetFullPath(Path.Combine(sAppPath, @"Data"));
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.Text += $" [{_sRootFolder}]";
+        }
+
+        private void frmMain_DragDrop(object sender, DragEventArgs e)
+        {
+            foreach (string sFolder in (string[])e.Data.GetData(DataFormats.FileDrop))
+            {
+                try
+                {
+                    if (Directory.Exists(sFolder))
+                    {
+                        if (File.Exists(Path.Combine(sFolder, "index.html")))
+                        {
+                            textBox_Log.AppendText($"Processing {sFolder}" + Environment.NewLine);
+                            FileInfo fi_Temp = new FileInfo(Path.Combine(_sRootFolder,
+                                String.Format("Temp-{0}.zip", Guid.NewGuid().ToString())));
+                            fi_Temp.Directory.Create();
+                            ZipFile.CreateFromDirectory(sFolder, fi_Temp.FullName);
+                            String sFilename = GetFilename(File.OpenRead(fi_Temp.FullName));
+                            FileInfo fi_Final = new FileInfo(Path.Combine(_sRootFolder, sFilename));
+                            if (!fi_Final.Exists)
+                            {
+                                fi_Final.Directory.Create();
+                                File.Move(fi_Temp.FullName, fi_Final.FullName);
+                                textBox_Log.AppendText($"  Save to {sFilename}" + Environment.NewLine);
+                            }
+                            else
+                            {
+                                textBox_Log.AppendText($"  Conflict - File Already Exists, {sFilename}" + Environment.NewLine);
+                                fi_Temp.Delete();
+                            }
+                        }
+                        else
+                        {
+                            textBox_Log.AppendText($"Skip {sFolder}, no index.html." + Environment.NewLine);
+                        }
+                    }
+                    else
+                    {
+                        textBox_Log.AppendText($"Skip {sFolder}, it is not a folder." + Environment.NewLine);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    textBox_Log.AppendText($"Exception: {ex.Message}, StackTrace: {ex.StackTrace}" + Environment.NewLine);
+                }
+            }
+        }
+
+        private void frmMain_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private String GetFilename(Stream stream)
+        {
+            using (stream)
+            {
+                SHA1Managed sha = new SHA1Managed();
+                byte[] checksum = sha.ComputeHash(stream);
+                return GetFilename(Base32Encoding.Standard.GetString(checksum));
+            }
+        }
+
+        private String GetFilename(String sHash)
+        {
+            try
+            {
+                byte[] checksum = Base32Encoding.Standard.ToBytes(sHash);
+                List<String> subfolders = new List<String>();
+                string subfolder = BitConverter.ToString(checksum).Replace("-", string.Empty);
+                int index = 0;
+                int total = subfolder.Length;
+                while (index < total)
+                {
+                    subfolders.Add(subfolder.Substring(index, Math.Min(4, total - index)));
+                    index += 4;
+                }
+                return String.Format(@"{0}\{1}.zip",
+                    String.Join(@"\", subfolders.ToArray()), sHash);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
